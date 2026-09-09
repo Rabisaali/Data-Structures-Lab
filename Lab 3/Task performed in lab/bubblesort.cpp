9a66830e57bb6bad397fc13e034e7c8a4b988148
Rabisa <rabisa55511@gmail.com>
2026-09-09 08:46:00 +0500

    uploading bubble sort code

---
+++ Lab 3/Task performed in lab/bubblesort.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int* BubbleSort(int* arr, int n) {
+    for(int i=0; i<n; i++) {
+        for (int j=0; j<n-1; j++) {
+            if (arr[j]>arr[j+1]) {
+                int temp = arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+int main () {
+    int n;
+    cout << "Enter number of elemnts of array: ";
+    cin >> n;
+    int* arr = new int[n];
+
+    for(int i=0; i<n; i++) {
+        cout << "Enter element: " << i+1 << ": ";
+        cin >> arr[i];
+    }
+
+    arr = BubbleSort(arr, n);
+
+    cout << "Sorted Array is: " << "\n";
+    for(int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    delete[] arr;
+}
