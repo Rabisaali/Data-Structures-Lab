7ef462eb499258079be7c9e5d94fd0acd0404dc9
Rabisa <rabisa55511@gmail.com>
2026-09-16 09:08:18 +0500

    uploading linkedlist code

---
+++ Lab 4/Tasks performed in lab/LinkedList.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data=val;
+        next=NULL;
+    }
+};
+
+class Singly {
+    public:
+        Node* head;
+        Node* tail;
+
+        Singly() {
+            head=NULL;
+            tail=NULL;
+        }
+
+        void insertAt(int pos, int val) {
+            if(pos<0) {
+                cout << "Invalid position\n";
+                return;
+            }
+            Node* newNode = new Node(val);
+            if(pos==0) {
+                newNode->next=head;
+                head=newNode;
+                return;
+            }
+            Node* temp=head;
+            for(int i=0; i<pos-1; i++) {
+                if(temp==NULL) {
+                    cout << "Invalid position\n";
+                    return;
+                }
+                temp=temp->next;
+            }
+
+            
+            newNode->next = temp->next;
+            temp->next=newNode;
+        }
+
+        void display() {
+            Node* temp=head;
+            while(temp!=NULL) {
+                cout << temp->data << " ";
+                temp=temp->next;
+            }
+        }
+};
+
+int main () {
+    Singly s;
+    s.insertAt(0, 9);
+    s.insertAt(1, 8);
+    s.insertAt(2, 18);
+    s.insertAt(0, 7);
+    s.display();
+}
+
